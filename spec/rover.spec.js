533c025e77c68f804aaf5c070cebcac91b3318c8
Jonathan Byers <byersj920@gmail.com>
2023-10-23 20:08:14 -0500

    Added test 10 and 11

--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -22,7 +22,8 @@ let testRover1 = new Rover(123456789);
 
   /* Test 8 */
   test('response returned by receiveMessage contains the name of the message', function(){
-    let testingMessage = new Message('This is a test! Do not be alarmed!');
+    let singleCommand = new Command('STATUS_CHECk');
+    let testingMessage = new Message('This is a test! Do not be alarmed!', singleCommand);
     let response = testRover1.receiveMessage(testingMessage);
     expect(response.message).toEqual('This is a test! Do not be alarmed!');
   });
@@ -39,11 +40,42 @@ let testRover1 = new Rover(123456789);
 
   /* Test 10 */
   test('responds correctly to the status check command', function(){
-    /* This first expect tests to make sure STATUS_CHECK is an object. Then, I individually make sure each key of the object is
-    returning correctly*/
-    let commandTest3 = new Command('STATUS_CHECK');
-    let testingMessage2 = new Message('Test 10 test', commandTest3);
-    testRover1.receiveMessage(testingMessage2);
-    expect(typeof testRover1.results).toEqual('object');
+    /* This test will use the same test Message object from Test 9. It will evaluate if both the completed condition is present
+    as well as if the STATUS_CHECK command is an object and also contains the right stuff. */
+    let commandTest1 = new Command('STATUS_CHECK');
+    let commandTest2 = new Command('MODE_CHANGE');
+    let commandCombined = [commandTest1, commandTest2];
+    let testingMessage = new Message('Another test baybee', commandCombined);
+    let response = testRover1.receiveMessage(testingMessage);
+    /*Copied Stuff ^*/
+    expect(response.results[0].completed).toEqual(true);
+    expect(typeof response.results[0].roverStatus).toEqual('object');
+    expect(response.results[0].roverStatus.mode).toEqual('NORMAL');
+    expect(response.results[0].roverStatus.position).toEqual(123456789);
+    expect(response.results[0].roverStatus.generatorWatts).toEqual(110);
   });
+
+/* Test 11 */
+  test('responds correctly to the mode change command', function(){
+    /* This test will use the same test Message as the previous two, but will give the MODE_CHANGE command a value this time. */
+    let commandTest1 = new Command('STATUS_CHECK');
+    let commandTest2 = new Command('MODE_CHANGE', 'LOW_POWER'); /* <- Testing this! */
+    let commandCombined = [commandTest1, commandTest2];
+    let testingMessage = new Message('Another test baybee', commandCombined);
+    let response = testRover1.receiveMessage(testingMessage);
+    /* Mostly copied stuff ^ */
+    expect(response.results[0].completed).toEqual(true);
+    expect(testRover1.mode).toEqual('LOW_POWER');
+});
+
+/* Test 12 */
+test('responds with a false completed value when attempting to move in LOW_POWER mode', function(){
+  /* In this test, testRover is still in LOW_POWER mode from test 11. So it should NOT be able to move. */
+  let commandTest1 = new Command('MOVE', 987654321);
+  let testingMessage = new Message('Movement Test', commandTest1);
+  let response = testRover1.receiveMessage(testingMessage);
+  expect(testRover1.mode).toEqual('LOW_POWER');
+  expect(response.results[0].completed).toEqual(false);
+});
+
 });
